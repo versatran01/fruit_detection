7a238842ab0f42c5d49ef3f60c6434a7e1775e39
gareth-cross <gcross.code@icloud.com>
2015-02-10 08:52:28 -0500

    Added names as return value

--- tools/loadImages.m
+++ tools/loadImages.m
@@ -1,10 +1,11 @@
-function [ images ] = loadImages( path, pattern, verbose )
+function [ images, names ] = loadImages( path, pattern, verbose )
 %LOADIMAGES Load all images (recursively) that match a file pattern.
 % Example patterns:
 %   '.+.(tiff|tif|jpg|jpeg|png|bmp|gif)' : Most common image files.
 %   'img_[0-9]+.jpg' : JPG files with names like "img_001.jpg".
 items = dir(path);
 images = {};
+names = {};
 if nargin < 3
     verbose = false;
 end
@@ -19,16 +20,21 @@ for i=1:numel(items)
     if items(i).isdir
         % ignore the current directory and parent
         if ~strcmp(name,'.') && ~strcmp(name,'..')
-            sub = loadImages(strcat(path,'/',name), pattern, verbose);
+            [sub, name] = loadImages(strcat(path,'/',name), pattern,...
+                verbose);
             images = vertcat(images, sub);
+            names = vertcat(names, name);
         end
     else
         matches = regexpi(name,pattern,'match');
         if ~isempty(matches)
             % found a match
-            images{end+1} = imread(strcat(path,'/',name));
+            fullpath = strcat(path,'/',name);
+            images{end+1} = imread(fullpath);
+            names{end+1} = fullpath;
         end
     end
 end
-images = reshape(images,numel(images),1);
+images = reshape(images, numel(images), 1);
+names = reshape(names, numel(names), 1);
 end
