050e98efea083c06999f7f628028d444398a7123
gareth-cross <gcross.code@icloud.com>
2015-02-24 08:39:46 -0500

    Added script for calculating descriptors on sample image

---
+++ scripts/demoDescriptors.m
@@ -0,0 +1,16 @@
+%% demoDescriptors.m
+init;
+
+%% load images
+images = loadImages(DATASET_PATH,[]);
+
+%% load descriptors
+load('descriptors/kmeans.mat');
+displayPatches(models{1}.weights, true);
+
+%% apply descriptors
+filtered = applyDescriptors(images{96}, models{1});
+
+%% preview image (14,41,12,25)
+figure;
+imshow(filtered(:,:,25));
