9bf6fdac1211f68f9a51a0a1988262a240ce7c44
Chao Qu <quchao@seas.upenn.edu>
2015-02-26 17:14:17 -0500

    add predictLiblinear

--- predict/predictLiblinear.m
+++ predict/predictLiblinear.m
@@ -1,6 +1,24 @@
-function Yhat = predictLiblinear(model, Xtest)
+function Yhat = predictLiblinear(model, Xtest, clamp)
 % PREDICTLIBLINEAR Generate predictions using a liblinear model
+%  Predict Yhat for Xtest using liblinearpredict with any liblinear model
 %
-% output:
+% INPUT:
+%  model - liblinear model
+%  Xtest - testing instance matrix
+%  clamp - clamp predicted label between [0,1]
 %
+% OUTPUT:
+%  Yhat  - predicted label
+
+if nargin < 3, clamp = true; end
+
+N = size(Xtest, 1);
+Yhat = liblinearpredict(ones(N, 1), Xtest, model, '-q');
+
+% Clamp predicted labels
+if clamp
+	Yhat(Yhat > 1) = 1;
+	Yhat(Yhat < 0) = 0;
+end
+
 end
