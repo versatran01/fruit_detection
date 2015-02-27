eab79d87112ed2691b5e1bfdb057ee6797c6a3fd
Chao Qu <quchao@seas.upenn.edu>
2015-02-27 18:48:21 -0500

    a better generateObservations script

--- scripts/generateObservations.m
+++ scripts/generateObservations.m
@@ -1,28 +1,55 @@
-%% generateObservations.m
-init;
-RATIO = 1;  % ratio of negative to positive training examples
-DESCRIPTORS_PATH = 'descriptors/kmeans.mat';
-IMAGE_SCALE = 0.5;
+function observations = generateObservations(do_save)
+% GENERATEOBSERVATIONS Generate observations by prompting user to select
+% dataset directory and descriptor file
 
-%% load dataset
-N = dataset.size();
+if nargin < 1, do_save = false; end
 
-% scale all images down...
-for i=1:N
-    dataset.images{i} = imresize(dataset.images{i}, IMAGE_SCALE);
+% Load dataset
+dataset_path = uigetdir('', 'Select dataset directory');
+if ~dataset_path
+    error('No dataset selected');
+end
+dataset = Dataset(dataset_path);
+
+% Load descriptors
+[descriptors_file, descriptors_path] = uigetfile('*.mat', ...
+                                                 'Select descriptor file');
+if ~descriptors_file
+    error('No descriptors selected');
 end
 
-%% generate feature space
+descriptors = load([descriptors_path, descriptors_file]);
+descriptors = descriptors.descriptors;
+
+observations = generate_observations(dataset, descriptors, 0.5, 1);
+
+% Save descriptors
+if do_save
+    save(sprintf('observations_%s.mat', date), 'observations');
+end
+
+end
+
+function observations = generate_observations(dataset, descriptors, ...
+                                              image_scale, ratio)
+%GENERATE_OBSERVATIONS 
+
+n_data = dataset.size();
+
+% Scale all images
+for i = 1:n_data
+    dataset.images{i} = imresize(dataset.images{i}, image_scale);
+end
+
+% Generate feature space
 Xpos = {};
 Xneg = {};
-parfor i=1:N
+parfor i = 1:n_data
     fprintf('Processing image %i\n...', i);
-    % process the image
-    % todo: assume one model for now, add support for multi-scale later
     desc = applyDescriptors(dataset.images{i}, descriptors{1});
     % sample the examples
-    [Xp,Xn] = sampleExamples(desc,...
-        dataset.selections{i}, dataset.masks{i}, RATIO, IMAGE_SCALE);
+    [Xp, Xn] = sampleExamples(desc, dataset.selections{i}, ...
+                              dataset.masks{i}, ratio, image_scale);
     Xpos{i} = Xp;
     Xneg{i} = Xn;
 end
@@ -30,9 +57,9 @@ end
 Xpos = cell2mat(Xpos');
 Xneg = cell2mat(Xneg');
 
-%% save
 observations.Xpos = Xpos;
 observations.Xneg = Xneg;
-observations.descriptors_path = DESCRIPTORS_PATH;
 observations.datetime = datetime;
-save(sprintf('observations_%s.mat', date), 'observations');
+
+end
+
