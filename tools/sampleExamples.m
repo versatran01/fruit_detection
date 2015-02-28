7fde0068502ff41be9a26ff7aa2f054fcf4eb6c2
gareth-cross <gcross.code@icloud.com>
2015-02-27 20:35:00 -0500

    Modified sample examples to support positive/negative labels

--- tools/sampleExamples.m
+++ tools/sampleExamples.m
@@ -1,22 +1,25 @@
 function [ Xpos, Xneg ] = sampleExamples( image, selections, masks, ...
-    ratio, scale )
+   scale )
 %SAMPLEEXAMPLES Sample examples (pixel level) from an
 % image. The size of the feature-space will be equal to the number of 
 % channels in the image.
 % `image` is input image.
 % `selections` are selections from dataset.
 % `masks` are masks from dataset.
-% `ratio` is ratio of negative to positive examples.
 C = size(image, 3);
 image = im2double(image);
 % create two large masks - one for positive and one for negative
 sz = size(image);
 
-mask_pos = createLargeMask(sz, selections, masks, 'scale', scale);
-
-[mask_pos,mask_neg] = createLargeMask(sz, selections, masks, scale);
+% mask fruit
+mask_pos = createLargeMask(sz, selections, masks,...
+    'scale', scale, 'filter', 1);
+% mask non-fruit
+mask_neg = createLargeMask(sz, selections, masks,...
+    'scale', scale, 'filter', 2, 'warnOnEmptyMask', false);
 mask_pos = logical(mask_pos);
-mask_neg = ~logical(mask_neg);  % flip here to get negative mask
+mask_neg = logical(mask_neg);
+
 % sample positive examples
 npos = nnz(mask_pos);
 Xpos = zeros(npos,C);
@@ -25,17 +28,12 @@ for i=1:C
     chan = chan(mask_pos);
     Xpos(:,i) = chan;
 end
-% sample negative examples
-nneg = floor(npos*ratio);
+% negative
+nneg = nnz(mask_neg);
 Xneg = zeros(nneg,C);
-% generate sample positions for negative examples
-idx = find(mask_neg);
-idx = idx(randperm(numel(idx)));
-idx = idx(1:nneg);
-% extract negatives
 for i=1:C
     chan = image(:,:,i);
-    chan = chan(idx);
+    chan = chan(mask_neg);
     Xneg(:,i) = chan;
 end
 end
