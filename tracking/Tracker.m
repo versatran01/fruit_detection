bee252e7f241f9bd97c948937283cf395e4f247d
gareth-cross <gcross.code@icloud.com>
2015-03-01 18:51:02 -0500

    Added empty file for tracker

---
+++ tracking/Tracker.m
@@ -0,0 +1,14 @@
+classdef Tracker < handle
+    %TRACKED 
+    %   todo: implement class for tracking fruit
+    %   maybe implement this as abstract base for different tracking
+    %   systems
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
