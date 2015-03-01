5cc26a244c855a69f1c75d5a78ad152f49faaf40
Chao Qu <quchao@seas.upenn.edu>
2015-03-01 18:11:46 -0500

    add tuneEnsemble to generate weights for each model

--- tune/tuneEnsemble.m
+++ tune/tuneEnsemble.m
@@ -1,20 +1,67 @@
-function ensemble_result = tuneEnsemble(model_names)
+function ensemble_result = tuneEnsemble(observations, model_names)
 
 % model_names - cell array of strings of model names that can be found in
 %               folder models. If not specified or empty, tuneEnsemble will
 %               try to use all the models that can be found
 
-if nargin < 1, model_names = {}; end
-loadModels(model_names);
+if nargin < 2, model_names = {}; end
+models = loadModels(model_names);
 
+Xtrain = observations.Xtrain;
+Xvalid = observations.Xtest;
 
+Ytrain = observations.Ytrain;
+Yvalid = observations.Ytest;
 
+Ytrain_hat = [];
+Yvalid_hat = [];
+
+valid_rmse = [];
+
+for i = 1:length(models)
+    model = models{i};
+    Ytrain_n = model.predict(model.model, Xtrain(:, model.feat_ind));
+    Yvalid_n = model.predict(model.model, Xvalid(:, model.feat_ind));
+    Ytrain_hat = [Ytrain_hat Ytrain_n];
+    Yvalid_hat = [Yvalid_hat Yvalid_n];
+    valid_rmse(i) = rootMeanSquare(Yvalid_n - Yvalid);
+    fprintf('Validation RMSE for %s -> %f.\n', model.name, valid_rmse(end));
+end
+
+ensemble_rmse = rootMeanSquare(Yvalid - mean(Yvalid_hat, 2));
+weights = 1./valid_rmse;
+weights = weights / sum(weights);
+weighted_rmse = rootMeanSquare(Yvalid - sum(bsxfun(@times, Yvalid_hat, weights), 2));
+resgression_weight = inv(Ytrain_hat' * Ytrain_hat + 1000 * eye(size(Ytrain_hat, 2))) ...
+                     * Ytrain_hat' * Ytrain;
+regression_rmse = rootMeanSquare(Yvalid - Yvalid_hat * resgression_weight);
+
+fprintf('\n=========================================');
+fprintf('\n ** Average Ensemble RMSE: %.4f **\n', ensemble_rmse)
+fprintf('=========================================');
+fprintf('\n ** Weighted Ensemble RMSE: %.4f **\n', weighted_rmse)
+fprintf('=========================================');
+fprintf('\n ** Regression Ensemble RMSE: %.4f **\n', regression_rmse)
+fprintf('=========================================\n');
 
 end
 
 function models = loadModels(model_names, models_dir)
 if nargin < 2, models_dir = 'models'; end
 
-listing = dir(models_dir)
+listings = dir(models_dir);
+
+models = {};
+k = 1;
+for i = 1:numel(listings)
+    listing = listings(i);
+    if ~listing.isdir
+        if ~isempty(strfind(listing.name, '.mat'))
+            result = load([models_dir, '/', listing.name]);
+            models{k} = result.result;
+            k = k + 1;
+        end
+    end
+end
 
 end
--- tune/tuneLiblinearModels.m
+++ tune/tuneLiblinearModels.m
@@ -1,23 +1,28 @@
-function tuneLiblinearModels(observations, tune_model_types)
+function tuneLiblinearModels(observations, tune_model_types, feat_ind)
 
 nlevel = 2;
 nfolds = 5;
 models_dir = 'models';
 
-[model_types, model_names, model_short_names] = getLiblinearTypes();
-if nargin < 2, tune_model_types = [0, 1, 2, 5, 7]; end
+[model_types, model_names, model_acronyms] = getLiblinearTypes();
+if nargin < 2, tune_model_types = 0:7; end
+if nargin < 3, feat_ind = [1:6, 11, 12]; end
 
 for i = 1:length(tune_model_types)
     model_type = tune_model_types(i);
     model_ind = find(model_types == model_type);
     model_name = model_names{model_ind};
     fprintf('Tunning %s.\n', model_name);
-    model_short_name = model_short_names{model_ind};
+    model_acronym = model_acronyms{model_ind};
     % Tune each individual model
-    result = tuneLiblinear(observations.Xtrain, observations.Ytrain, ...
+    result = tuneLiblinear(observations.Xtrain(:, feat_ind), ...
+                           observations.Ytrain, ...
                            nlevel, nfolds, model_type);
+    result.name = model_name;
+    result.acronym = model_acronym;
+    result.feat_ind = feat_ind;
     % Save model to disk
-    model_path = [models_dir, '/', model_short_name];
+    model_path = [models_dir, '/', model_acronym];
     save(model_path, 'result');
     fprintf('Model saved to %s.\n', model_path);
 end
