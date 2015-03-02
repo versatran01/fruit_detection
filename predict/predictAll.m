9f61cfce122c58ba9cfbe27e7a9c21ed21603a59
gareth-cross <gcross.code@icloud.com>
2015-03-02 09:43:42 -0500

    Commit for merge

--- predict/predictAll.m
+++ predict/predictAll.m
@@ -2,11 +2,10 @@ function [ Y ] = predictAll( model, X )
 %PREDICTALL Classify pixels using a tuned model.
 % `model` is the result of a tuning method (libSVM, libLinear, etc)
 % `X` is NxD input pixels. N pixels, in D-dimensional feature space.
-N = size(X,1);
 D = size(X,2);
-is_ensemble = true;    % todo: add support for ensemble model
-if is_ensemble
-    Y = model.predict(model, X);
+if isfield(model, 'sub_models')
+    % this is an ensemble
+    Y = predictEnsemble(model, X);
 else
     if model.dimension ~= D
         error('Model was trained on a different dimension feature space');
--- predict/predictEnsemble.m
+++ predict/predictEnsemble.m
@@ -6,8 +6,9 @@ num_models = length(model.sub_models);
 
 for i = 1:num_models
     sub_model = model.sub_models{i};
-    X_used = X(:, sub_model.feat_ind);
-    Yhat = sub_model.predict(sub_model.param, X_used);
+    %X_used = X(:, sub_model.feat_ind);
+    Yhat = predictAll(sub_model, X);
+    %sub_model.predict(sub_model.param, X_used);
     Yhat_all = [Yhat_all Yhat];
 end
 
