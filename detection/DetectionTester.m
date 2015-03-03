4c7c326fb020f800d6da43309656973a4ce80f2a
gareth-cross <gcross.code@icloud.com>
2015-03-03 09:27:48 -0500

    Added basic plotting to tester

--- detection/DetectionTester.m
+++ detection/DetectionTester.m
@@ -22,11 +22,13 @@ classdef DetectionTester < handle
             if isempty(self.hFig)
                 % create new UI
                 self.hFig = figure;
-                set(self.hFig,'Name','Detection Tester');
+                set(self.hFig, 'Name', 'Detection Tester');
                 self.hPlots(1) = subplot(1,2,1);
                 self.hImage = imshow(image);
+                hold on;
                 self.hPlots(2) = subplot(1,2,2);
                 self.hMask = imshow(mask);
+                hold on;
             else
                 % update old plots
                 set(0,'CurrentFigure', self.hFig);
@@ -34,9 +36,38 @@ classdef DetectionTester < handle
                 set(self.hMask, 'CData', mask);
             end
         end
+        
         function plotSelections(self, selections)
+            if ~isempty(self.hSelections)
+                delete(self.hSelections);
+                self.hSelections = [];
+            end
+            for i=1:numel(selections)
+                sel = selections{i};
+                if sel(4) ~= 1
+                    % only plot fruit selections
+                    continue;
+                end
+                points = createCirclePoints(sel(1:2), sel(3));
+                self.hSelections(i) = plot(points(:,1),points(:,2));
+                set(self.hSelections(i),'LineWidth',2);
+                set(self.hSelections(i),'Color',[1 0 0]);
+            end
         end
+        
         function plotDetections(self, detections)
+            if ~isempty(self.hDetections)
+                delete(self.hDetections);
+                self.hDetections = [];
+            end
+            for i=1:size(detections,1)
+                pts = bboxToLinePoints(detections(i,:));
+                pts_x = squeeze(pts(:,1,:));
+                pts_y = squeeze(pts(:,2,:));
+                self.hDetections(i) = plot(pts_x,pts_y);
+                set(self.hDetections(i),'LineWidth',2);
+                set(self.hDetections(i),'Color',[0 0 1]);
+            end
         end
     end
     
@@ -65,7 +96,8 @@ classdef DetectionTester < handle
             
             if self.viz
                 self.plotImage(image, mask);
-                
+                self.plotSelections(selections);
+                self.plotDetections(bbox);
             end
         end
     end
