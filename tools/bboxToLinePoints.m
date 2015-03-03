9a6d1916a03aa7934924fadcacd70f2b95d3903a
Chao Qu <quchao@seas.upenn.edu>
2015-03-03 18:59:38 -0500

    better bounding box visualization

--- tools/bboxToLinePoints.m
+++ tools/bboxToLinePoints.m
@@ -1,4 +1,4 @@
-function [ points ] = bboxToLinePoints( bbox )
+function [ X, Y ] = bboxToLinePoints( bbox )
 %BBOXTOLINEPOINTS Convert Nx4 vector of lines points to a 5x2xN vector of
 % line coordinates (for plotting).
 if isempty(bbox)
@@ -9,6 +9,7 @@ N = size(bbox,1);
 if size(bbox,2) ~= 4
     error('bbox must be Nx4');
 end
+
 points = zeros(5,2,N);
 for i=1:N
     box = bbox(i,:);
@@ -19,4 +20,5 @@ for i=1:N
               box(1:2) + [0 0]];
     points(:,:,i) = pts;
 end
+
 end
