cf5e592b3f53f2fe044f4be0a297ab93d548e7b7
gareth-cross <gcross.code@icloud.com>
2015-03-04 13:28:47 -0500

    Working more on detection

--- detection/detectFruit.m
+++ detection/detectFruit.m
@@ -3,28 +3,48 @@ function [ mask, bbox ] = detectFruit( pixel_model, image )
 % `pixel_model` is the detection model trained at the pixel level.
 % `image` is the input image in the RGB color space.
 
+% NOTE: all these parameters tuned at scale of 1
+
 % convert to extended color space
 image = rgb2fullcs(image);
 % calculate the mask with the pixel-level model
 mask = detectPixels(pixel_model, image);
-
-% todo: connected components, etc...
 mask = mask > 0.5;
 
-CC = bwconncomp(mask);
+% fill holes
+mask = imfill(mask,'holes');
 
-properties = regionprops(CC,'Area','BoundingBox','Centroid');
-area = [properties.Area];
-bbox = vertcat(properties.BoundingBox);
+% erode a bit
+se = strel('disk', 1);
+mask = imerode(mask, se);
 
-% throw away the pixels below the treshold
-idx = area > 40;
-strip = cell2mat( CC.PixelIdxList(~idx)' );
-mask(strip) = false;
+% find connected components
+CC = ConnectedComponents(mask);
 
-% get bounding boxes
-bbox = bbox(idx,:);
+% find properties of connected components
+area = CC.Area();
+bbox = CC.BoundingBox();
+centroids = CC.Centroid();
 
-% todo: more stuff
+% throw away components below a very low threshold of area
+large = area > 3;
+CC.discard(~large);
+
+% get bounding boxes and areas for remaining regions
+area = area(large,:);
+bbox = bbox(large,:);
+centroids = centroids(large,:);
+
+% calculate distance between centroids
+dist = pdist2(centroids, centroids, 'cityblock');
+
+nearby = dist < 30;
+nearby = triu(nearby,1);    % take everything above the diagonal only
+[row,col] = find(nearby);
+
+[unique_rows,ia,ic] = unique(row);
+
+% row,col are now groups we should form
+
 
 end
