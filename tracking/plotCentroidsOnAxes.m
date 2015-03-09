97817ff7947a17236d0e0e0b60519d451370a2fa
Chao Qu <quchao@seas.upenn.edu>
2015-03-09 14:35:47 -0400

    improved visualization

---
+++ tracking/plotCentroidsOnAxes.m
@@ -0,0 +1,14 @@
+function handle = plotCentroidsOnAxes(ax, handle, centroids, line_spec, width)
+if nargin < 6, width = 1; end
+
+if isempty(handle) || ~isgraphics(handle)
+    hold(ax, 'on');
+    handle = plot(ax, centroids(:, 1), centroids(:, 2), line_spec, ...
+                  'LineWidth', width);
+    hold(ax, 'off');
+else
+    set(handle, 'XData', centroids(:, 1), 'YData', centroids(:, 2));
+end
+
+end
+
--- tracking/plotImageOnAxes.m
+++ tracking/plotImageOnAxes.m
@@ -1,5 +1,4 @@
 function [ handle ] = plotImageOnAxes(ax, handle, image)
-%PLOTIMAGEONEAXES 
 if isempty(handle) || ~isgraphics(handle)
     disp('Creating new image handle');
     handle = imshow(image, 'Parent', ax);
