915a35660e8b078e8d5e2305634ab74b0f5891c8
gareth-cross <gcross.code@icloud.com>
2015-03-09 09:54:33 -0400

    Added flag to disable test_tracker plots

--- test_tracker.m
+++ test_tracker.m
@@ -8,14 +8,17 @@ else
 end
 use_pause = true;
 plot_tracker = true;
+plot_detections = false;
 bag.resetView(bag.topics);
 tracker = FruitTracker(plot_tracker);
 
-figure(1);
-handles(1) = subplot(1,2,1);
-handles(2) = subplot(1,2,2);
-imhandles = {[],[]};
-patchhandle = [];
+if plot_detections
+    figure(1);
+    handles(1) = subplot(1,2,1);
+    handles(2) = subplot(1,2,2);
+    imhandles = {[],[]};
+    patchhandle = [];
+end
 count = 1;
 
 while bag.hasNext()
@@ -30,28 +33,30 @@ while bag.hasNext()
         
         tracker.track(CC, image);
         
-        if isempty(imhandles{1})
-            imhandles{1} = imshow(image, 'Parent', handles(1));
-        else
-            set(imhandles{1}, 'cdata', image);
+        if plot_detections
+            if isempty(imhandles{1})
+                imhandles{1} = imshow(image, 'Parent', handles(1));
+            else
+                set(imhandles{1}, 'cdata', image);
+            end
+            set(handles(1), 'YDir', 'normal');
+            hold on;
+
+            if isempty(patchhandle)
+                patchhandle = patch(X, Y, 'r', 'Parent', handles(1), ...
+                      'EdgeColor', 'r', 'FaceAlpha', '0.1');
+            else
+                set(patchhandle,'xdata',X,'ydata',Y);
+            end
+
+            % mask
+            if isempty(imhandles{2})
+                imhandles{2} = imshow(CC.image, 'Parent', handles(2));
+            else
+                set(imhandles{2}, 'cdata', CC.image);
+            end
+            set(handles(2), 'YDir', 'normal');
         end
-        set(handles(1), 'YDir', 'normal');
-        hold on;
-        
-        if isempty(patchhandle)
-            patchhandle = patch(X, Y, 'r', 'Parent', handles(1), ...
-                  'EdgeColor', 'r', 'FaceAlpha', '0.1');
-        else
-            set(patchhandle,'xdata',X,'ydata',Y);
-        end
-        
-        % mask
-        if isempty(imhandles{2})
-            imhandles{2} = imshow(CC.image, 'Parent', handles(2));
-        else
-            set(imhandles{2}, 'cdata', CC.image);
-        end
-        set(handles(2), 'YDir', 'normal');
         fprintf('Processed image %i\n', count);
         count = count+1;
         drawnow;
