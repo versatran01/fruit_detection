6409e62db162f25bc4c4781cc1fdfa75aae10974
gareth-cross <gcross.code@icloud.com>
2015-03-14 20:14:19 -0400

    Moved csvwrite into tools

---
+++ yield/generateYieldMap.m
@@ -0,0 +1,9 @@
+function [ map ] = generateYieldMap( bag_dir, row_counts )
+%GENERATEYIELDMAP Generate a yield map, given a directory of bagfiles
+%`bag_dir` and a set of incremental row counts with timestamps,
+%`row_counts`.
+
+
+
+end
+
