d3c588c8d1730fedfe47e3a6da59a6c5b8a0b37d
Chao Qu <quchao@seas.upenn.edu>
2015-03-16 16:09:31 -0400

    remove preprocess folder

--- preprocess/generateObesrvationCs.m
+++
@@ -1,6 +0,0 @@
-function obs = generateObesrvationCs()
-
-ds = Dataset('/home/chao/Dropbox/agriculture_state_of_art/Datasets/booth/booth_combined');
-obs = extractObservations(ds, @rgb2fullcs);
-end
-
